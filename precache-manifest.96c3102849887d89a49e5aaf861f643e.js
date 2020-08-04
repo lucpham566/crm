95248d513b66d1de635bca4702901ec09cd3103d
pham tien luc <lucpham566@gmail.com>
2020-08-04 18:59:33 +0700

    ad

---
+++ precache-manifest.96c3102849887d89a49e5aaf861f643e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0c0b52ff93fa8692bdc99c7403a641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d718790f60badbc3cf",
+    "url": "/static/js/2.79e35a04.chunk.js"
+  },
+  {
+    "revision": "af5111307236c4c69d6adba51f98588a",
+    "url": "/static/js/2.79e35a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8daad73e097759f4cb19",
+    "url": "/static/js/main.74aab596.chunk.js"
+  },
+  {
+    "revision": "571e0690116d05c07f0f",
+    "url": "/static/js/runtime-main.8b60a04f.js"
+  },
+  {
+    "revision": "1ac6a3f4567925772167fd94a7003898",
+    "url": "/static/media/loading.1ac6a3f4.gif"
+  }
+]);
